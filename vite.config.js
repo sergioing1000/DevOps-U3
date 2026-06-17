3f33a4b8e09d3d9209b62387d133debf5dc145ea
Sergio Cruz <sergioing@yahoo.com>
2026-06-17 18:42:11 -0500

    Fix Vite and Vitest configuration 	modified:   vite.config.js 	modified:   vitest.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-
-    test: {
-
-        globals: true,
-
-        environment: "jsdom",
-
-        setupFiles: "./src/setupTests.js"
-
-    }
-
+  plugins: [react()],
 });
